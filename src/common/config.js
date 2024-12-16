5c5ee41671da64bc0ac664d7e475655de1838577
ioj4 <69911332+ioj4@users.noreply.github.com>
2024-12-16 18:33:08 +0100

    bump

--- src/common/config.js
+++ src/common/config.js
@@ -6,7 +6,7 @@ export const srcDir = dirname(dirname(fileURLToPath(import.meta.url)));
 
 export const startTime = Date.now();
 
-export const version = "12";
+export const version = "13";
 
 let rawCfg;
 try {
