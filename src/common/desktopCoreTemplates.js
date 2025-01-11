eb4ffa96587a6f14c5177d72525e2fa272525496
ioj4 <69911332+ioj4@users.noreply.github.com>
2025-01-11 17:55:48 +0100

    [core-template] block @sentry/electron requests

--- src/common/desktopCoreTemplates.js
+++ src/common/desktopCoreTemplates.js
@@ -33,7 +33,41 @@ export const finalizeDesktopCoreIndex = (patches, hasPreload) =>
 	`// SHELTUPDATE PATCH FRAMEWORK INDEX HEADER:
 {
 const electron = require("electron");
+const stream = require("stream");
 const path = require("path");
+
+// Block Sentry requests
+// We create stubs for electron.net.request to make Sentry think that the requests succeeded.
+// Failed Sentry requests get added to a queue on disk. Meaning that if the user were to
+// uninstall sheltupdate, all the requests would still be sent.
+// see https://github.com/getsentry/sentry-electron/blob/3e4e10525b5fb24ffa98b211b91393f81e3555be/src/main/transports/electron-net.ts#L64
+// and https://github.com/getsentry/sentry-electron/blob/3e4e10525b5fb24ffa98b211b91393f81e3555be/src/main/transports/offline-store.ts#L52
+
+class RequestStub extends stream.Writable {
+  _write(chunk, encoding, cb) {
+    cb();
+  }
+  setHeader() {}
+  on(type, cb) {
+    if (type !== "response") return;
+    cb({
+      on: () => {},
+      headers: {},
+      statusCode: 200,
+    });
+  }
+}
+
+const origRequest = electron.net.request;
+electron.net.request = function (options) {
+  if (!options?.hostname?.endsWith("sentry.io")) {
+    return origRequest.apply(this, arguments);
+  }
+  console.log("[sheltupdate] Blocking Sentry request!");
+  return new RequestStub();
+};
+
+
 ${hasPreload ? preloadScaffold : ""}
 }
 
