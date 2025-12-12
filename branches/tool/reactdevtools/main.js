68c308c54998f727f63ca60b81d7bdebb79598e9
xirreal <me@xirreal.dev>
2025-12-12 21:51:44 +0100

    update react devtools ext and make it load on first boot

--- branches/tool/reactdevtools/main.js
+++ branches/tool/reactdevtools/main.js
@@ -1,6 +1,21 @@
 const { app, session } = require("electron");
 const { join } = require("path");
 
+// apparently, manifest v3 extensions don't have their service workers started automatically?
+// https://github.com/electron/electron/issues/41613#issuecomment-2644018998
+function launchExtensionBackgroundWorkers(session) {
+	return Promise.all(
+		session.getAllExtensions().map(async (extension) => {
+			const manifest = extension.manifest;
+			if (manifest.manifest_version === 3 && manifest?.background?.service_worker) {
+				await session.serviceWorkers.startWorkerForScope(extension.url);
+				console.log("[sheltupdate-react-devtools] Manually starting background worker");
+			}
+		}),
+	);
+}
+
 app.whenReady().then(async () => {
 	await session.defaultSession.loadExtension(join(__dirname, "ext"));
+	await launchExtensionBackgroundWorkers(session.defaultSession);
 });
