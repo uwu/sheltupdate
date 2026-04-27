7505d11dcf6e5f5110debe57cc1f18b62d187423
xirreal <me@xirreal.dev>
2026-04-27 11:33:01 +0200

    make non-release images have a marquee bar

--- src/dashboard/index.js
+++ src/dashboard/index.js
@@ -12,6 +12,9 @@ const js__ = readFileSync(join(srcDir, "dashboard", "dashboard.js"), "utf8");
 const hitRatio = ({ hit, miss }) => (hit || miss ? ((100 * hit) / (hit + miss)).toFixed(1) + "%" : "N/A");
 
 const escape = (str) => str.replaceAll("&", "&amp;").replaceAll("<", "&lt;").replaceAll(">", "&gt;");
+const isRelease = ![undefined, "false", "0"].includes(process.env.SHELTUPDATE_RELEASE);
+const stagingMarquee = `<div id="marquee-bar"><span id="marquee-text">${"STAGING  ".repeat(60)}</span></div>`;
+
 function clusterStatusTemplate() {
 	const nodes = getClusterHealth();
 	return `
@@ -42,7 +45,8 @@ function template(temp) {
 		.replaceAll("__CACHE_PROX__", hitRatio(statsState.proxyCacheHitRatio))
 		.replaceAll("__CACHE_V1__", hitRatio(statsState.v1ModuleCacheHitRatio))
 		.replaceAll("__CACHE_V2__", hitRatio(statsState.v2ManifestCacheHitRatio))
-		.replaceAll("__CLUSTER_STATUS__", () => (config.discovery.enabled ? clusterStatusTemplate() : ""));
+		.replaceAll("__CLUSTER_STATUS__", () => (config.discovery.enabled ? clusterStatusTemplate() : ""))
+		.replaceAll("__STAGING_MARQUEE__", isRelease ? "" : stagingMarquee);
 }
 
 export default new Hono()
