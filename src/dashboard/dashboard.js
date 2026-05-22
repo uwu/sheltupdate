1dbea8125f33e030d29a6d61c44627f13d15187c
kasimir <kasimir@k6.tf>
2026-05-23 00:01:05 +0200

    this sucks

--- src/dashboard/dashboard.js
+++ src/dashboard/dashboard.js
@@ -1,6 +1,6 @@
 // for some reason esm.sh needs bundle-deps for this. probably helps bundle size anyway.
 // full bundle: 391.25kB, partial bundle: 295.68kB. its something!
-import * as Plot from "@observablehq/plot?standalone&exports=plot,barX,groupY,groupZ,text";
+import * as Plot from "@observablehq/plot@0.6.17?standalone&exports=plot,barX,groupY,groupZ,text";
 // full bundle: 82.36kB, partial bundle: 22.23kB <- this is very inaccurate since this pulls in a bunch more files
 import {
 	format,
